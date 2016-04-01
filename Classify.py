1432e57b285b4eb56455379b92cd9f91ba66cf15
GarethTucker <garethtucker2012@gmail.com>
2016-04-01 17:15:48 +1300

    Sending it home, home on teh range...

---
+++ Classify.py
@@ -0,0 +1,9 @@
+
+class Classify (object):
+    def __init__(self, data):
+        self.images = data.images
+        self.construct_features()
+
+    def construct_features(self):
+        print (self.images)
+
--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-
+from Classify import Classify
 
 class Main (object):
     def __init__(self, perceptron_file):
@@ -8,19 +8,20 @@ class Main (object):
             line_number = 0;
             while line_number < len(lines):
                 if len(lines[line_number]) > 10:
-                    line = lines[line_number] + lines[line_number+1]
-                    print ('line: %s' %line)
-                    self.images.append(line)
+                    line = lines[line_number].rstrip() + lines[line_number+1].rstrip()
+                    start_break_point = 0
+                    end_break_point = 10
+                    self.image = list()
+                    while end_break_point <= len(line):
+                        image_row = line[start_break_point:end_break_point]
+                        self.image.append(image_row)
+                        end_break_point += 10
+                        start_break_point += 10
+                    self.images.append(self.image)
                     line_number += 2
                 else:
                     line_number += 1
-            print('number of images: %d' %len(self.images))
-            print('2d array test 1: %s' %self.images[1][2])
-            print('2d array test 2: %s' %self.images[2][len(self.images[2])-2])
-            print('2d array test 3: %s' %self.images[1][len(self.images[1])-4])
-
-
-
-
 if __name__=="__main__":
     data = Main('image.data')
+
+    Classify(data)
